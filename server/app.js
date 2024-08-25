035045cbf2f304ed2eea7f8340a5305de2ac48c3
Amit Regev <regevamit@gmail.com>
2024-08-25 20:05:12 +0300

    Fixes

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors'); // Add this line
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -19,6 +20,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Enable CORS for all routes
+app.use(cors()); // Add this line
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
